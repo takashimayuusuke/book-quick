de73d376a03e265807374442a1cd7ad3d7959d6f
takashimayuusuke <taka.z34.vq37@gmail.com>
2021-01-11 16:44:35 +0900

    bookモデルバリデーション設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,11 @@
 class Book < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validate :title
+    validate :link_to_buy
+    validate :story
+    validate :review
+    validate :genre_id
+  end
 end
