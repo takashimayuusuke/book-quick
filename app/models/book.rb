77a8c48f5584b49c3c57effe2ff49efc6e5584d3
takashimayuusuke <taka.z34.vq37@gmail.com>
2021-01-12 11:51:32 +0900

    bookモデル修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,3 @@
 class Book < ApplicationRecord
   belongs_to :user
-
-  with_options presence: true do
-    validate :title
-    validate :link_to_buy
-    validate :story
-    validate :review
-    validate :genre_id
-  end
 end
