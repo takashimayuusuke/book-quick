733f7f1bb4293f0b6d61a40f95cbc93fcc255dab
takashimayuusuke <taka.z34.vq37@gmail.com>
2021-01-13 14:23:52 +0900

    アクティブハッシュ実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,14 @@
 class Book < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecoedExtensions
+  belongs_to :genre
+
+
+  with_options presence: true do
+  validates :title
+  validates :story
+  validates :review
+  validates :genre_id, numericality: { other_than: 1 }
+  end
 end
