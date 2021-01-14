78a4ee69059cf78833eaea565bde1f4bbf145e62
takashimayuusuke <taka.z34.vq37@gmail.com>
2021-01-14 11:15:12 +0900

    booksコントローラー、新規投稿のアクションの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,26 @@
 class BooksController < ApplicationController
 
   def index
+    @books = BookAuthorObject.includes(:user).order('created_at DESC')
   end
   
   def new
+    @book_author_object = BookAuthorObject.new
+  end
+
+  def create
+    @book_author_object = BookAuthorObject.new(book_author_object_params)
+      if @book_author_object.valid?
+        @book_author_object.save
+        redirect_to root_path(@book_author_object)
+      else
+        render action: :new
+      end
+  end
+
+  private
+
+  def book_author_object_params
+    params.require(:book_author_object).permit(:title, :story, :review, :genre_id, :image, :name).merge(user_id: current_user.id)
   end
 end
